0138ca766ec2fffd8fbcd091b3e2871c8dbc756e
msolivera <32486643+msolivera@users.noreply.github.com>
2022-01-04 23:38:05 -0300

    Fin jornada
    Commit final de jornada

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"log"
 
 	"github.com/msolivera/socialTwittorBackEnd/bd"
+	"github.com/msolivera/socialTwittorBackEnd/handlers"
 )
 
 func main() {
 	if bd.ChequeoConexion() == 0 {
 		log.Fatal("Sin conexion")
+		return
 	}
+	handlers.Manejadores()
 
 }
